719159e1f06a179cd63117d4d73c8027d5f1ae20
ZTHLLY <zzz18011940139@gmail.com>
2024-04-29 01:16:08 +0800

    picture crawler devlop and test by si1v3r in 2024/4/29

--- si1v3rsearch-backend/src/test/java/com/yupi/springbootinit/crawlerTest.java
+++ si1v3rsearch-backend/src/test/java/com/yupi/springbootinit/crawlerTest.java
@@ -9,24 +9,47 @@ import cn.hutool.json.JSONUtil;
 
 import com.yupi.springbootinit.model.entity.Post;
 import com.yupi.springbootinit.service.PostService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
-public class hutoolHttpTest {
+public class crawlerTest {
 
     @Resource
     private PostService postService;
+    @Test
+    void crawlImg() throws IOException {
+
+        String url = "https://www.bing.com/images/search?q=星穹铁道黄泉&first=1";
+        Document doc = Jsoup.connect(url).get();
+        System.out.println(doc);
+        Elements elements = doc.select(".iuscp.isv");
+        for (Element element : elements) {
+            //图片地址murl
+            String m = element.select(".iusc").get(0).attr("m");
+            Map<String,String> map=JSONUtil.toBean(m,Map.class);
+            String murl= map.get("murl");
+            //图片标题
+            String title=element.select(".inflnk").get(0).attr("aria-label");
+            System.out.println(title);
+            //System.out.println(murl);
+        }
+    }
 
     @Test
-    public void httpTest(){
+     void httpTest(){
         //1.获取数据
         String json ="{\"current\":1,\"pageSize\":8,\"sortField\":\"createTime\",\"sortOrder\":\"descend\",\"category\":\"文章\",\"tags\":[],\"reviewStatus\":1}";
         String url = "https://www.code-nav.cn/api/post/search/page/vo";
