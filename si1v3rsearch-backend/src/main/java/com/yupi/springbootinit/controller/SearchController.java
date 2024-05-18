47d67d019117ea94b677ef42769cecbcd83ee2d0
ZTHLLY <zzz18011940139@gmail.com>
2024-05-19 03:13:01 +0800

    search controller backend by si1v3r in 2024/5/19

--- si1v3rsearch-backend/src/main/java/com/yupi/springbootinit/controller/SearchController.java
+++ si1v3rsearch-backend/src/main/java/com/yupi/springbootinit/controller/SearchController.java
@@ -2,18 +2,25 @@ package com.yupi.springbootinit.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yupi.springbootinit.common.BaseResponse;
+import com.yupi.springbootinit.common.ErrorCode;
 import com.yupi.springbootinit.common.ResultUtils;
+import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.springbootinit.exception.ThrowUtils;
 import com.yupi.springbootinit.model.dto.post.PostQueryRequest;
 import com.yupi.springbootinit.model.dto.search.SearchAllRequest;
 import com.yupi.springbootinit.model.dto.user.UserQueryRequest;
 import com.yupi.springbootinit.model.entity.Picture;
+import com.yupi.springbootinit.model.enums.SearchTypeEnum;
 import com.yupi.springbootinit.model.vo.PostVO;
 import com.yupi.springbootinit.model.vo.SearchVO;
 import com.yupi.springbootinit.model.vo.UserVO;
 import com.yupi.springbootinit.service.PictureService;
 import com.yupi.springbootinit.service.PostService;
 import com.yupi.springbootinit.service.UserService;
+
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +28,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 
 @RestController
@@ -40,27 +49,87 @@ public class SearchController {
 
     @PostMapping("/all")
     public BaseResponse<SearchVO> searchAll(@RequestBody SearchAllRequest searchAllRequest, HttpServletRequest request){
-        //图片搜索
+
+
+        //找出搜索类型
+        String type = searchAllRequest.getType();
+        SearchTypeEnum enumByValue = SearchTypeEnum.getEnumByValue(type);
+        //ThrowUtils.throwIf(StringUtils.isBlank(type),ErrorCode.PARAMS_ERROR);
+
+        //取出搜索词
         String searchText=searchAllRequest.getSearchText();
-        Page<Picture> pictureList = pictureService.PictureSearch(searchText, 1, 10);
 
-        //用户搜索
-        UserQueryRequest userQueryRequest = new UserQueryRequest();
-        userQueryRequest.setUserName(searchText);
-        Page<UserVO> userList = userService.listUserVOByPage(userQueryRequest);
+        if(enumByValue==null){
+            //新建返回对象
+            SearchVO searchVO=new SearchVO();
+            //并发
+            CompletableFuture<Page<Picture>> pictureTask= CompletableFuture.supplyAsync(()->{
+                //图片搜索
+                Page<Picture> pictureList = pictureService.PictureSearch(searchText, 1, 10);
+                return pictureList;
+            });
 
-        //帖子搜索
-        PostQueryRequest postQueryRequest = new PostQueryRequest();
-        postQueryRequest.setSearchText(searchText);
-        Page<PostVO> postList = postService.listPostVOByPage(postQueryRequest, request);
+            CompletableFuture<Page<UserVO>> userTask= CompletableFuture.supplyAsync(()->{
+                //用户搜索
+                UserQueryRequest userQueryRequest = new UserQueryRequest();
+                userQueryRequest.setUserName(searchText);
+                Page<UserVO> userList = userService.listUserVOByPage(userQueryRequest);
+                return userList;
+            });
 
-        SearchVO searchVO=new SearchVO();
-        searchVO.setPictureList(pictureList.getRecords());
-        searchVO.setPostVOList(postList.getRecords());
-        searchVO.setUserVOList(userList.getRecords());
+            //帖子搜索 这个没有并发
+            PostQueryRequest postQueryRequest = new PostQueryRequest();
+            postQueryRequest.setSearchText(searchText);
+            Page<PostVO> postList = postService.listPostVOByPage(postQueryRequest, request);
 
 
-        return ResultUtils.success(searchVO);
+            CompletableFuture.allOf(userTask,pictureTask).join();
+
+            try {
+
+                Page<Picture> pictureList = pictureTask.get();
+                Page<UserVO> userList = userTask.get();
+                searchVO.setPictureList(pictureList.getRecords());
+                searchVO.setPostVOList(postList.getRecords());
+                searchVO.setUserVOList(userList.getRecords());
+                return ResultUtils.success(searchVO);
+            }catch (Exception e){
+                log.error("查询异常",e);
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR,"查询异常");
+            }
+        }else {
+            //新建返回对象
+            SearchVO searchVO=new SearchVO();
+            searchVO.setPostVOList(new ArrayList<>());
+            searchVO.setUserVOList(new ArrayList<>());
+            searchVO.setPictureList(new ArrayList<>());
+
+            switch (enumByValue) {
+                case POST:
+                    PostQueryRequest postQueryRequest = new PostQueryRequest();
+                    postQueryRequest.setSearchText(searchText);
+                    Page<PostVO> postList = postService.listPostVOByPage(postQueryRequest, request);
+                    searchVO.setPostVOList(postList.getRecords());
+                    break;
+                case USER:
+                    UserQueryRequest userQueryRequest = new UserQueryRequest();
+                    userQueryRequest.setUserName(searchText);
+                    Page<UserVO> userList = userService.listUserVOByPage(userQueryRequest);
+                    searchVO.setUserVOList(userList.getRecords());
+                    break;
+                case PICTURE:
+                    Page<Picture> pictureList = pictureService.PictureSearch(searchText, 1, 10);
+                    searchVO.setPictureList(pictureList.getRecords());
+                    break;
+                default:
+            }
+
+            return ResultUtils.success(searchVO);
+        }
+
+
+
+
     }
 
 
